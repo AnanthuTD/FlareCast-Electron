78d3df92cb5cc1f4ef4c8e2ce22557a6e9276cd5
AnanthuTD <ananthutd2021@gmail.com>
2025-04-19 10:27:24 +0530

    feat: updated sonner

--- src/renderer/src/studio_main.tsx
+++ src/renderer/src/studio_main.tsx
@@ -3,9 +3,11 @@ import './assets/main.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import StudioApp from './StudioApp'
+import { Toaster } from './components/ui/sonner'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Toaster />
     <StudioApp />
   </React.StrictMode>
 )
--- src/renderer/src/webcam_main.tsx
+++ src/renderer/src/webcam_main.tsx
@@ -3,9 +3,11 @@ import './assets/main.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import WebcamApp from './WebcamApp'
+import { Toaster } from './components/ui/sonner'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Toaster />
     <WebcamApp />
   </React.StrictMode>
 )
